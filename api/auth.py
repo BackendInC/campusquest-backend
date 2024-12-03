fba1a4792295e526d2c0d468b429c4fa5e5df9d5
Kenan Bandaliyev <kenan.bandaliyev@gmail.com>
2024-12-03 19:04:20 +0300

    feat: add middleware and refactor users

---
+++ api/auth.py
@@ -0,0 +1,38 @@
+import jwt
+from datetime import datetime, timedelta
+from fastapi import HTTPException, Security
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+
+# Secret key to sign the token
+SECRET_KEY = "your-secret-key"
+FORMAT = "%Y-%m-%d %H:%M:%S"
+
+
+# Function to generate a JWT
+def generate_jwt(user_id, secret_key=SECRET_KEY):
+    payload = {
+        "user_id": user_id,
+        "expires": (datetime.utcnow() + timedelta(minutes=1)).strftime(FORMAT),
+    }
+
+    token = jwt.encode(payload, secret_key, algorithm="HS256")
+    return token
+
+
+# HTTP Bearer token scheme
+security = HTTPBearer()
+
+
+# Function to decode a JWT
+def decode_jwt(credentials: HTTPAuthorizationCredentials = Security(security)) -> int:
+    try:
+        payload = jwt.decode(credentials.credentials, SECRET_KEY, algorithms=["HS256"])
+        print(payload)
+
+        if datetime.strptime(payload["expires"], FORMAT) < datetime.utcnow():
+            raise HTTPException(status_code=401, detail="Token has expired!")
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(status_code=401, detail="Token has expired!")
+    except jwt.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Invalid token!")
