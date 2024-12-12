c71e7111f8d9377b8fbae4cb21f23d6bbc4b75ad
MHD kamal rushdi <rushdi21@itu.edu.tr>
2024-12-12 18:56:25 +0300

    added the correct app.py and api/__init__.py from other user-login branch

--- api/__init__.py
+++ api/__init__.py
@@ -7,11 +7,12 @@ import db.schemas as schemas
 # routers for each api prefix
 from api.achievements import router as achievementsRouter
 from api.users import router as usersRouter
-
+from api.quests import router as questsRouter
 
 app = FastAPI()
 app.include_router(achievementsRouter)
 app.include_router(usersRouter)
+app.include_router(questsRouter)
 
 
 def start_app():
