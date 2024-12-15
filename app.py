577d9c03bb606cb16ae22c49b160e55c694d9d21
danaaljasem <danaaljasem74@gmail.com>
2024-12-15 09:30:48 +0300

    removed logs

--- app.py
+++ app.py
@@ -1,19 +1,6 @@
-from fastapi import FastAPI
+from api import app
 import db
-from api.users import router as users_router
-from api.achievements import router as achievements_router
-from api.posts import router as posts_router
 
-app = FastAPI()
 
-# Root endpoint
-@app.get("/")
-def read_root():
-    return {"message": "Welcome to CampusQuest API!"}
-
-# Include your routers here
-app.include_router(users_router)
-app.include_router(achievements_router)
-app.include_router(posts_router)
-
+# Initialize the database
 db.Base.metadata.create_all(bind=db.engine)
