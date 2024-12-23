f7ad532d25dc343f55cf11ec31ab30414c3f8044
Kenan Bandaliyev <kenanbanda2004@gmail.com>
2024-12-23 14:31:46 +0300

    update test.sh

--- test.sh
+++ test.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-pip install -r requirements.txt
+echo "Installing requirements..."
+pip install -r requirements.txt >/dev/null 2>&1
 
 docker compose -f compose.dev.yaml down
 docker rm -f postgres-test >/dev/null 2>&1
 docker run --name postgres-test -e POSTGRES_PASSWORD=1234 -e POSTGRES_USER=root -e POSTGRES_DB=backendinc -p 5432:5432 -d postgres
 
+echo "Waiting for postgres to start..."
 sleep 3
+
+echo "Running tests..."
 python3 -m pytest
